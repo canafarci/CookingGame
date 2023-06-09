2ee1407a59885ee9d6361d2da1e3a8e3affc3a88
canafarci <ismetberke@gmail.com>
2023-06-10 00:01:15 +0300

    input system refactor

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    PlayerInputActions _playerInputActions;
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+    }
+    public Vector2 GetInputVectorNormalized()
+    {
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        return inputVector;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,28 +6,16 @@ public class Player : MonoBehaviour
 {
     public bool IsWalking { get { return _isWalking; } }
     [SerializeField] private float _moveSpeed, _rotateSpeed;
+    private GameInput _gameInput;
     private bool _isWalking;
+    private void Awake()
+    {
+        _gameInput = FindObjectOfType<GameInput>();
+    }
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1f;
-        }
+        Vector2 inputVector = _gameInput.GetInputVectorNormalized();
 
-        inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * Time.deltaTime * _moveSpeed;
 
