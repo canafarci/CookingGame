0faa7665da67951821800b8f94a9f6a659e4529f
canafarci <ismetberke@gmail.com>
2023-06-10 01:46:16 +0300

    checking for obstacles when player moves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     public bool IsWalking { get { return _isWalking; } }
     [SerializeField] private float _moveSpeed, _rotateSpeed;
+    const float PLAYER_RADIUS = 0.7f;
+    const float PLAYER_HEIGHT = 2f;
     private GameInput _gameInput;
     private bool _isWalking;
     private void Awake()
@@ -16,12 +18,50 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = _gameInput.GetInputVectorNormalized();
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * Time.deltaTime * _moveSpeed;
-
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * _rotateSpeed);
-
         //set variables
-        _isWalking = inputVector != Vector2.zero;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        float moveDistance = _moveSpeed * Time.deltaTime;
+        //check can move
+        bool canMove = CanMove(moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            //cant move, try only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = CanMove(moveDirX, moveDistance);
+            if (canMove)
+            {
+                //update vector
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //if cant move on X, try Z
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = CanMove(moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    //update direction
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
+        _isWalking = canMove && inputVector != Vector2.zero;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * _rotateSpeed);
+    }
+
+    bool CanMove(Vector3 moveDir, float moveDistance)
+    {
+        return !Physics.CapsuleCast(transform.position,
+                                 transform.position + Vector3.up * PLAYER_HEIGHT,
+                                PLAYER_RADIUS,
+                                moveDir,
+                                moveDistance);
     }
 }
