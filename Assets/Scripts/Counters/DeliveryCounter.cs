fbd66c3ee7b40e30e60f0f9cea8147a2f6375408
canafarci <ismetberke@gmail.com>
2023-06-19 16:55:57 +0300

    Delivery counter visual shader

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        //if player has a KO and that KO is a plate
+        if (player.HasKitchenObject() && player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+        {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
